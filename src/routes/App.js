1303324814312cb180e920701e8158b8298e4c02
argianardi <argianardi1435@gmail.com>
2022-11-08 16:14:55 +0700

    add route dan home page

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,10 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "../pages/Home";
 import "../styles/App.css";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
