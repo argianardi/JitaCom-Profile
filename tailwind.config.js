84c4a2ccf958a9891fdae06fe6fbb6d1a13f45e9
argianardi <argianardi1435@gmail.com>
2022-11-08 14:31:16 +0700

    add colors and fonts (roboto & poppins)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        biruJita: "#1B345F",
+        orangeJita: "#F7731C",
+        putihJita: "#F0F4FD",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
