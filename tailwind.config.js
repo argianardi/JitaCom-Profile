870f5caed8752fafb02da2b9b2382990b81b37b0
herbayulet <sayaherbayu@gmail.com>
2022-11-09 08:00:13 +0700

    customize screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        biruJita: "#1B345F",
-        orangeJita: "#F7731C",
-        putihJita: "#F0F4FD",
-      },
-      fontFamily: {
-        roboto: ["Roboto", "sans-serif"],
-        poppins: ["Poppins", "sans-serif"],
-      },
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        screens: {
+            sm: "320px",
+            // => @media (min-width: 640px) { ... }
+
+            md: "768px",
+            // => @media (min-width: 768px) { ... }
+
+            lg: "1024px",
+            // => @media (min-width: 1024px) { ... }
+
+            xl: "1280px",
+            // => @media (min-width: 1280px) { ... }
+
+            "2xl": "1536px",
+            // => @media (min-width: 1536px) { ... }
+        },
+        extend: {
+            colors: {
+                biruJita: "#1B345F",
+                orangeJita: "#F7731C",
+                putihJita: "#F0F4FD",
+            },
+            fontFamily: {
+                roboto: ["Roboto", "sans-serif"],
+                poppins: ["Poppins", "sans-serif"],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
