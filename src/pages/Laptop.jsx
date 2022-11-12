566930f9887339fc9176714f11fad81db4eeddf0
argianardi <argianardi1435@gmail.com>
2022-11-12 23:59:10 +0700

    add jumbotron

--- src/pages/Laptop.jsx
+++ src/pages/Laptop.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Header from "../components/Header";
+import jumbotronImg from "../img/laptop/jumbotronLaptop.png";
 
 export const Laptop = () => {
   return (
     <>
-      <p>laptop page</p>
+      <div className="bg-[url('../img/laptop/jumbotronLaptop.png')] relative  bg-center  pb-12">
+        <div className="absolute w-full h-full opacity-75 bg-biruJita"></div>
+        <Header />
+        <p className="relative my-5 text-xl font-bold text-center text-white 2sm:my-20 2sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-8xl font-roboto">
+          Laptop Original dan Bergaransi
+        </p>
+      </div>
+
+      <div>
+        <p className="bg-[#d9d9d9] inline-block p-2 rounded-b-xl text-biruJita font-bold md:text-xl md:p-3">
+          Harga kompetitif dan berkualitas
+        </p>
+      </div>
     </>
   );
 };
