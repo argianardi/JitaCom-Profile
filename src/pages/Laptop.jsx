c740aeb738db540410a65098818500f0fbb447ad
argianardi <argianardi1435@gmail.com>
2022-11-12 23:59:58 +0700

    add footer

--- src/pages/Laptop.jsx
+++ src/pages/Laptop.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Footer } from "../components/Footer";
 import Header from "../components/Header";
 import jumbotronImg from "../img/laptop/jumbotronLaptop.png";
 
@@ -18,6 +19,8 @@ export const Laptop = () => {
           Harga kompetitif dan berkualitas
         </p>
       </div>
+
+      <Footer />
     </>
   );
 };
