f2324815dcfcc0a6bd9827c2afa739f35e8a9ee5
argianardi <argianardi1435@gmail.com>
2022-11-12 20:52:32 +0700

    add image dahua product

--- src/pages/CCTV.jsx
+++ src/pages/CCTV.jsx
@@ -1,10 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // import { Jumbotron } from "../components/Jumbotron";
 import cctvInhp from "../img/cctv/cctv-in-hp.png";
+import dahuaPaket2 from "../img/cctv/dahuaPaket2.jpg";
+import dahuaPaket4 from "../img/cctv/dahuaPaket4.jpg";
+import dahuaPaket6 from "../img/cctv/dahuaPaket6.jpg";
+import dahuaPaket8 from "../img/cctv/dahuaPaket8.jpg";
+import dahuaPaket16 from "../img/cctv/dahuaPaket16.jpg";
+
 import Header from "../components/Header";
 import { Footer } from "../components/Footer";
+import { DataCCTV } from "../data/DataCCTV";
 
 export const CCTV = () => {
+  const [dataCCTV, setDataCCTV] = useState([
+    {
+      title: "DAHUA IR 4 2MP",
+      fullTitle: "DAHUA PAKETAN 4 CAMERA 2MP IR DVR 4/8 CH",
+      camIndoor: "2 Camera Indoor",
+      camOutdoor: "2 Camera Outdoor",
+      dvr: "DVR 4 Chanel",
+      psu: "Power Suplay 10 A",
+      rg59: "Kabel 100M",
+      conector: "BNC CCTV",
+      powerCamera: "DC CCTV",
+      hdd: "HDD 1TB (Perekaman 1 Bulan)",
+      kabelDisplay: "Kabel HDMI/VGA 1,5M",
+      jasa: "Jasa Instalasi & Configurasi",
+      harga4CH: 3800000,
+      harga8CH: 4150000,
+      img: dahuaPaket2,
+    },
+    {
+      title: "DAHUA IR 4 2MP",
+      fullTitle: "DAHUA PAKETAN 4 CAMERA 2MP IR DVR 4/8 CH",
+      camIndoor: "2 Camera Indoor",
+      camOutdoor: "2 Camera Outdoor",
+      dvr: "DVR 4 Chanel",
+      psu: "Power Suplay 10 A",
+      rg59: "Kabel 100M",
+      conector: "BNC CCTV",
+      powerCamera: "DC CCTV",
+      hdd: "HDD 1TB (Perekaman 1 Bulan)",
+      kabelDisplay: "Kabel HDMI/VGA 1,5M",
+      jasa: "Jasa Instalasi & Configurasi",
+      harga4CH: 3800000,
+      harga8CH: 4150000,
+      img: dahuaPaket2,
+    },
+  ]);
+  // useEffect(() => {
+  //   setDataCCTV(DataCCTV);
+  // }, []);
+  console.log(dataCCTV);
+  console.log(dataCCTV[0].img);
   return (
     <div>
       <div className="bg-[url('../img/jumbotronCctv.png')] relative  bg-center  ">
@@ -32,6 +80,7 @@ export const CCTV = () => {
         <h2 className="text-3xl font-bold text-center font-roboto text-biruJita">
           Paket CCTV
         </h2>
+        <img src={dataCCTV[0].img} alt="" />
       </div>
 
       <Footer />
