d920ed488adcfdb63ef35b08b9d17908f3352d51
argianardi <argianardi1435@gmail.com>
2022-11-14 21:44:30 +0700

    fix jumbotron

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,10 +9,9 @@ import printerImg from "../img/printer/printer.png";
 export const Home = () => {
   return (
     <div>
-      <div className="bg-[url('../img/jumbotronHome.png')] bg-center  relative pb-3 sm:pb-10 md:pb-16">
-        <div className="absolute w-full h-full opacity-75 bg-biruJita"></div>
-        <Header />
-        <div className="relative my-10 text-xl text-center md:my-14 lg:my-20 xl:my-24 sm:text-3xl md:text-4xl xl:text-5xl ">
+      <Header />
+      <div className="w-full pb-3 -mt-20 border border-blue-900 home sm:pb-10 md:pb-16 ">
+        <div className="mt-24 mb-8 text-xl text-center sm:mt-32 sm:mb-14 md:mt-48 sm:text-3xl md:text-4xl xl:text-5xl">
           <p className="font-bold text-white ">
             Jita Com{" "}
             <span className="text-3xl font-bold md:text-5xl font-roboto">
