50983890ae2a30b4cdb5fb6eda21fdd67b45f2d3
argianardi <argianardi1435@gmail.com>
2022-11-14 21:32:50 +0700

    fix jumbotron

--- src/pages/Laptop.jsx
+++ src/pages/Laptop.jsx
@@ -6,15 +6,14 @@ import jumbotronImg from "../img/laptop/jumbotronLaptop.png";
 export const Laptop = () => {
   return (
     <>
-      <div className="bg-[url('../img/laptop/jumbotronLaptop.png')] relative  bg-center  pb-12">
-        <div className="absolute w-full h-full opacity-75 bg-biruJita"></div>
-        <Header />
-        <p className="relative my-5 text-xl font-bold text-center text-white 2sm:my-20 2sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-8xl font-roboto">
+      <Header />
+      <div className="w-full -mt-20 border border-blue-900 laptop">
+        <p className="relative mt-24 text-xl font-bold text-center text-white mb-14 sm:mt-32 sm:mb-24 sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-8xl font-roboto md:mt-36 md:mb-28 lg:mt-40 lg:mb-32">
           Laptop Original dan Bergaransi
         </p>
       </div>
 
-      <div>
+      <div className="mb-96">
         <p className="bg-[#d9d9d9] inline-block p-2 rounded-b-xl text-biruJita font-bold md:text-xl md:p-3">
           Harga kompetitif dan berkualitas
         </p>
--- src/pages/Printer.jsx
+++ src/pages/Printer.jsx
@@ -5,15 +5,14 @@ import Header from "../components/Header";
 export const Printer = () => {
   return (
     <>
-      <div className="bg-[url('../img/printer/jumbotronPrinter.png')] relative  bg-center  pb-12">
-        <div className="absolute w-full h-full opacity-75 bg-biruJita"></div>
-        <Header />
-        <p className="relative my-5 text-xl font-bold text-center text-white 2sm:my-20 2sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-8xl font-roboto">
-          Laptop Original dan Bergaransi
+      <Header />
+      <div className="w-full -mt-20 border border-blue-900 printer">
+        <p className="relative mt-24 text-xl font-bold text-center text-white mb-14 sm:mt-32 sm:mb-24 sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-8xl font-roboto md:mt-36 md:mb-28 lg:mt-40 lg:mb-32">
+          Printer Original dan Bergaransi
         </p>
       </div>
 
-      <div>
+      <div className="mb-96">
         <p className="bg-[#d9d9d9] inline-block p-2 rounded-b-xl text-biruJita font-bold md:text-xl md:p-3">
           Harga kompetitif dan berkualitas
         </p>
