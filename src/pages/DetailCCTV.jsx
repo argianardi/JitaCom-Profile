71bc8bb16e4f0b1dc17ad0c4d833a031b6ab990c
argianardi <argianardi1435@gmail.com>
2022-11-16 00:00:53 +0700

    add slicing detail page

--- src/pages/DetailCCTV.jsx
+++ src/pages/DetailCCTV.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { IoLogoWhatsapp } from "react-icons/io5";
+
 import { Footer } from "../components/Footer";
 import Header from "../components/Header";
 
@@ -10,69 +12,70 @@ export const DetailCCTV = () => {
       <Header />
       <div className="w-full py-10 -mt-20 border border-blue-900 cctv"></div>
 
-      <div className="bh">
-        <div className="br">
-          <img
-            src={location.state.img}
-            alt={`gambar ${location.state.fullTitle}`}
-          />
-        </div>
-        <div className="bg">
-          <h1>{location.state.fullTitle}</h1>
-          {location.state.harga4ch !== 0 && (
-            <p className="ml-9 text-biruJita dark:text-white">
-              Paket DVR 4 CH:{" "}
-              <span className="font-bold">
-                {Intl.NumberFormat("id-ID", {
-                  style: "currency",
-                  currency: "IDR",
-                  currencyDisplay: "symbol",
-                  minimumFractionDigits: 0,
-                }).format(location.state.harga4ch)}
-              </span>
-            </p>
-          )}
-          <p className=" text-biruJita ml-9 dark:text-white">
-            {" "}
-            Paket DVR 8 CH:{" "}
-            <span className="font-bold">
-              {Intl.NumberFormat("id-ID", {
-                style: "currency",
-                currency: "IDR",
-                minimumFractionDigits: 0,
-              }).format(location.state.harga8ch)}
-            </span>
-          </p>
-          <ul className="list-disc ml-9">
-            <li>{location.state.camIndoor}</li>
-            {location.state.camOutdoor !== null && (
-              <li>{location.state.camOutdoor}</li>
-            )}
-            <li>{location.state.dvr}</li>
-            <li>{location.state.psu}</li>
-            <li>{location.state.rg59}</li>
-            <li>{location.state.conector}</li>
-            <li>{location.state.powerCamera}</li>
-            <li>{location.state.hdd}</li>
-            <li>{location.state.kabelDisplay}</li>
-            <li>{location.state.jasa}</li>
-          </ul>
-        </div>
-        {/* <p>{location.state.fullTitle}</p>
-        <p>{location.state.camIndoor}</p>
-        <p>{location.state.camOutdoor}</p>
-        <p>{location.state.dvr}</p>
-        <p>{location.state.psu}</p>
-        <p>{location.state.rg59}</p>
-        <p>{location.conector}</p>
-        <p>{location.state.powerCamera}</p>
-        <p>{location.state.hdd}</p>
-        <p>{location.state.kabelDisplay}</p>
-        <p>{location.state.harga4ch}</p>
-        <p>{location.state.harga8ch}</p>
-        <img src={location.state.img} alt="" /> */}
-      </div>
+      <div className="px-2">
+        <div className="flex flex-col flex-wrap p-1 sm:p-2 my-20 rounded-md shadow-md md:flex-row text-biruJita shadow-black/60 w-full sm:max-w-[600px] md:max-w-[768px] lg:max-w-[1000px] mx-auto">
+          <div className="flex flex-col flex-wrap md:flex-row">
+            <div>
+              <img
+                src={location.state.img}
+                alt={`gambar ${location.state.fullTitle}`}
+                className="mx-auto h-72 "
+              />
+            </div>
+            <div className="ml-0 md:ml-3">
+              <h1 className="text-lg font-bold">{location.state.fullTitle}</h1>
+              {location.state.harga4ch !== 0 && (
+                <p className="mt-2 ml-3 dark:text-white">
+                  Paket DVR 4 CH:{" "}
+                  <span className="font-bold">
+                    {Intl.NumberFormat("id-ID", {
+                      style: "currency",
+                      currency: "IDR",
+                      currencyDisplay: "symbol",
+                      minimumFractionDigits: 0,
+                    }).format(location.state.harga4ch)}
+                  </span>
+                </p>
+              )}
+              <p className="ml-3 dark:text-white">
+                {" "}
+                Paket DVR 8 CH:{" "}
+                <span className="font-bold">
+                  {Intl.NumberFormat("id-ID", {
+                    style: "currency",
+                    currency: "IDR",
+                    minimumFractionDigits: 0,
+                  }).format(location.state.harga8ch)}
+                </span>
+              </p>
+              <ul className="list-disc ml-9">
+                <li>{location.state.camIndoor}</li>
+                {location.state.camOutdoor !== null && (
+                  <li>{location.state.camOutdoor}</li>
+                )}
+                <li>{location.state.dvr}</li>
+                <li>{location.state.psu}</li>
+                <li>{location.state.rg59}</li>
+                <li>{location.state.conector}</li>
+                <li>{location.state.powerCamera}</li>
+                <li>{location.state.hdd}</li>
+                <li>{location.state.kabelDisplay}</li>
+                <li>{location.state.jasa}</li>
+              </ul>
+            </div>
+          </div>
 
+          <div className="self-start mt-4 ml-0 md:self-end lg:ml-6">
+            <a
+              className="flex items-center justify-center w-48 p-1 m-1 rounded-lg bg-biruJita hover:bg-orangeJita"
+              href="https://wa.link/3507s0"
+            >
+              <IoLogoWhatsapp color="green" size={35} />
+              <p className="font-bold text-white ">Pesan Sekarang</p>
+            </a>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
