dae8c5cacaefd7bd178534b4fbbccdd37493b306
argianardi <argianardi1435@gmail.com>
2022-11-15 18:52:31 +0700

    add link for navigate to social media, online store and contacts

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,23 +16,32 @@ export const Footer = () => {
     <div className="px-5 py-3 text-white sm:px-3 md:px-10 lg:px-20 xl:px-28 2xl:px-44 bg-biruJita font-roboto">
       <div className="flex flex-wrap justify-between">
         <div className="mb-5 ">
-          <div className="mb-5">
+          <div className="mb-5 ">
             <h2 className="font-bold">Social Media</h2>
-            <div className="flex items-center ml-3">
-              <AiFillFacebook size={25} />
-              <p className="text-sm">Candra Jita Kom</p>
-            </div>
-            <div className="flex items-center ml-3">
+            <a
+              href="https://m.facebook.com/people/Candra-Jita-Kom/100078947852689/"
+              className="flex items-center ml-3 hover:text-orangeJita"
+            >
+              <AiFillFacebook size={25} className="" />
+              <p className="text-sm hover:underline">Candra Jita Kom</p>
+            </a>
+            <a
+              href="https://www.instagram.com/jita_komputer_bengkulu/"
+              className="flex items-center ml-3 hover:text-orangeJita"
+            >
               <AiFillInstagram size={25} />
-              <p className="text-sm">Jita_Komputer_Bengkulu</p>
-            </div>
+              <p className="text-sm hover:underline">Jita_Komputer_Bengkulu</p>
+            </a>
           </div>
           <div>
             <h2 className="font-bold">Toko Online</h2>
-            <div className="flex items-end ml-3">
+            <a
+              href="https://www.tokopedia.com/pgkom"
+              className="flex items-end ml-3 hover:text-orangeJita"
+            >
               <img src={logoTokped} alt="logo-tokopedia" />
-              <p className="text-sm">Pagoda Komputer</p>
-            </div>
+              <p className="text-sm hover:underline">Pagoda Komputer</p>
+            </a>
           </div>
         </div>
         <div>
@@ -44,25 +53,34 @@ export const Footer = () => {
                 JL. Mangga 04 No 09 Lingkar Timur Singaran Pati Kota Bengkulu
               </p>
             </div>
-            <div className="flex items-center">
+            <a
+              href="https://maps.app.goo.gl/JEVkygg7vRifLF2G9"
+              className="flex items-center hover:text-orangeJita"
+            >
               <img
                 src={logoGoogleMap}
                 alt="logo google map"
                 height={25}
                 width={25}
               />
-              <p className="text-sm ml-[0.5px]">Jita Computer</p>
-            </div>
+              <p className="text-sm ml-[0.5px] hover:underline">
+                Jita Computer
+              </p>
+            </a>
             <div className="flex items-center">
               <AiOutlineClockCircle size={25} />
               <p className="text-sm ml-[2px]">
                 Buka Setiap Hari, 08:00-20:00 WIB
               </p>
             </div>
-            <div className="flex items-center">
+            <a
+              target="_blank"
+              href="https://wa.link/3507s0"
+              className="flex items-center hover:underline hover:text-orangeJita"
+            >
               <AiOutlineWhatsApp size={25} />
               <p className="text-sm ml-[2px]">0812 7952 2663</p>
-            </div>
+            </a>
             <div className="flex items-center">
               <AiOutlinePhone size={25} />
               <p className="text-sm ml-[2px]">
